0ee2bf3ed2b257c5e8942060b5bd3601b541c7e0
Andrew Buttigieg <andrewbuttigieg@gmail.com>
2016-10-20 22:47:40 +0200

    Adds example.

---
+++ akka-cluster-router/Actors/SomeActor.cs
@@ -0,0 +1,17 @@
+﻿using Akka.Actor;
+using System;
+
+namespace AkkaClusterRouter.Actors
+{
+    public class SomeActor: ReceiveActor
+    {
+        public SomeActor(object bob)
+        {
+            Console.WriteLine(bob?.ToString());
+
+            Receive<object>( msg =>{
+                Console.WriteLine(msg?.ToString());
+            });
+        }
+    }
+}
---
+++ akka-cluster-router/Program.cs
@@ -0,0 +1,100 @@
+﻿using Akka.Actor;
+using Akka.Configuration;
+using Akka.Routing;
+using AkkaClusterRouter.Actors;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AkkaClusterRouter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             var seedConfig = @"
+akka {
+    actor {
+        provider = ""Akka.Cluster.ClusterActorRefProvider, Akka.Cluster""
+        deployment {
+            /talker {
+                router = broadcast-group
+                routees.paths = [""/user/someActor""]
+                nr-of-instances = 3
+                cluster {
+			        enabled = on
+					allow-local-routees = off
+	            }
+            }
+        }
+    }
+    remote {
+        helios.tcp {
+            transport-class = ""Akka.Remote.Transport.Helios.HeliosTcpTransport, Akka.Remote""
+            applied-adapters = []
+            transport-protocol = tcp
+            port = 50003
+            hostname = localhost
+        }
+    }
+    cluster {
+        seed-nodes = [""akka.tcp://clusterSystem@localhost:50003""]
+    }
+}";
+
+ var nonSeedConfig = @"
+akka {
+    actor {
+        provider = ""Akka.Cluster.ClusterActorRefProvider, Akka.Cluster""
+        deployment {
+        }
+    }
+    remote {
+        helios.tcp {
+            transport-class = ""Akka.Remote.Transport.Helios.HeliosTcpTransport, Akka.Remote""
+            applied-adapters = []
+            transport-protocol = tcp
+            port = 0
+            hostname = localhost
+        }
+    }
+    cluster {
+        seed-nodes = [""akka.tcp://clusterSystem@localhost:50003""]
+        roles = [""tracker""]
+    }
+}";
+
+            var config = ConfigurationFactory.ParseString(seedConfig);
+
+            using (var system = ActorSystem.Create("clusterSystem", config))
+            {
+                Console.WriteLine("Created System.");
+
+                var someActor = system.ActorOf(Props.Create(() => new SomeActor(12345)), "someActor");
+                var someActorThatDoesNotGetCreated = system.ActorOf(Props.Create(() => new SomeActor(someActor)), "someActorThatDoesNotGetCreated");
+                var router = system.ActorOf(Props.Empty.WithRouter(FromConfig.Instance), "talker");
+
+                system
+                   .Scheduler
+                   .ScheduleTellRepeatedly(TimeSpan.FromSeconds(0),
+                            TimeSpan.FromSeconds(5),
+                            router, 654321, ActorRefs.NoSender);
+
+                system
+                   .Scheduler
+                   .ScheduleTellRepeatedly(TimeSpan.FromSeconds(0),
+                            TimeSpan.FromSeconds(5),
+                            someActorThatDoesNotGetCreated, 654321, ActorRefs.NoSender);
+
+                var configNode = ConfigurationFactory.ParseString(nonSeedConfig);
+
+                using (var systemNode = ActorSystem.Create("clusterSystem", configNode))
+                {
+                    Console.ReadLine();
+                }
+            }
+        }
+    }
+}
